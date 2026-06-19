700e01f84c193e70ec634df142f63b2808b59d9e
MAshishSWTEmbeddedArchitect <50134279+MAshishSWT@users.noreply.github.com>
2026-06-19 12:44:09 +0530

    Build Success

---
+++ interface/soundbox/build/obj/src/sb_log.d
@@ -0,0 +1,2 @@
+build/obj/src/sb_log.o: src/sb_log.c \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_log.h
--- interface/soundbox/build/obj/src/sb_phase0_build_anchor.d
+++
@@ -1,2 +0,0 @@
-build/obj/src/sb_phase0_build_anchor.o: src/sb_phase0_build_anchor.c \
- D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_phase0_contract.h
