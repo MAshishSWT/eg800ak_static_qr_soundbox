4276a5d9d9a056fa0a42f50693b55fb91fbad8f9
MAshishSWTEmbeddedArchitect <50134279+MAshishSWT@users.noreply.github.com>
2026-06-21 16:45:47 +0530

    Build Success

--- interface/soundbox/build/obj/src/sb_app_main.d
+++ interface/soundbox/build/obj/src/sb_app_main.d
@@ -16,7 +16,9 @@ build/obj/src/sb_app_main.o: src/sb_app_main.c \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_event_bus.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_event.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_extnor.h \
- D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_log.h \
- D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_network_service.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_http_service.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_config.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_log.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_mqtt_service.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_network_service.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_supervisor.h
