3665ba5cbcc56a41c364546029a56cecb23c6d16
MAshishSWTEmbeddedArchitect <50134279+MAshishSWT@users.noreply.github.com>
2026-06-23 11:26:51 +0530

    Build Success

--- interface/soundbox/build/obj/src/sb_http_service.d
+++ interface/soundbox/build/obj/src/sb_http_service.d
@@ -24,6 +24,8 @@ build/obj/src/sb_http_service.o: src/sb_http_service.c \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_ssl_hal.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_rtos.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_type.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/fs/ql_fs.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/fs/lfs.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_rtos.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_ssl_hal.h \
  D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_business_service.h \
