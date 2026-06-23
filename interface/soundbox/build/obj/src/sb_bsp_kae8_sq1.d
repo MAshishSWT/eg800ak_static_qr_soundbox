b104442b2d56165236309e583d4e0d64a4649f29
MAshishSWTEmbeddedArchitect <50134279+MAshishSWT@users.noreply.github.com>
2026-06-23 21:01:47 +0530

    Build Success

---
+++ interface/soundbox/build/obj/src/sb_bsp_kae8_sq1.d
@@ -0,0 +1,18 @@
+build/obj/src/sb_bsp_kae8_sq1.o: src/sb_bsp_kae8_sq1.c \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_rtos.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_type.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_bsp_kae8_sq1.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_error.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_adc.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_type.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_event.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_event_bus.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_event.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_adc.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_audio_pa.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_key.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/common/include/ql_gpio.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_gpio.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_board_kae8_sq1.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_hal_led.h \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_log.h
---
+++ interface/soundbox/build/obj/src/sb_default_certs.d
@@ -0,0 +1,2 @@
+build/obj/src/sb_default_certs.o: src/sb_default_certs.c \
+ D:\GitHub\LTE01R07A16V03_C_SDK_A_SDK\ql-sdk\ql-application\threadx/interface/soundbox/include/sb_default_certs.h
